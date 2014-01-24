b90d6419aec00f402d3e68e03c264d26982a2220
Mackenzie Larson <larsonma@seawolf.sonoma.edu>
2014-01-23 18:04:29 -0800

    Added README.md

--- createWorkspace.sh
+++ createWorkspace.sh
@@ -1,7 +1,9 @@
 echo What is the project or lab title? 
 read ASSIGNMENT_TITLE 
  
- 
+finger $USER | grep Name | awk '{ print $4, $5 }' > README.md
+date >> README.md
+echo "ASSIGNMENT_TITLE" >> README.md  
 
 mkdir $ASSIGNMENT_TITLE
 mv README.md $ASSIGNMENT_TITLE
